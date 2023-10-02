42bb527050880bfd279d23fec247e43957249c59
antwoine <antwoinegriggs@outlook.com>
2023-10-02 16:56:06 -0400

    User, join, SaleOrder

--- server/models.py
+++ server/models.py
@@ -4,6 +4,26 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(128), nullable=False)
+
+    # Add relationships to associate users with their data (customers, sales orders, and inventory)
+    customers = db.relationship('Customer', backref='user', lazy=True)
+    sales_orders = db.relationship('SalesOrder', backref='user', lazy=True)
+    inventory = db.relationship('ProductInventory', backref='user', lazy=True)
+
+
+    # Serialization rules to control output
+    serialize_rules = ('-customers.user', '-sales_orders.user', '-inventory.user', '-password_hash')
+
+
+
 class ProductInventory(db.Model, SerializerMixin):
     __tablename__ = 'inventory'
 
@@ -31,3 +51,37 @@ class Customer(db.Model):
         'ProductInventory', secondary='customer_inventory_orders', overlaps='customer_inventory')
     # One-to-Many SaleOrders
     customer_orders = db.relationship('SalesOrder')
+
+class SalesOrder(db.Model):
+    __tablename__ = 'sales_orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    order_number = db.Column(db.String(255))
+
+    @property
+    def customer_name(self):
+        return self.product.name
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+
+    @property
+    def product_name(self):
+        return self.product.product_name
+    product_inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'))
+
+    quantity = db.Column(db.Integer)
+    amount = db.Column(db.Float)
+
+    # Many-to-Many Custoemr
+    customer_order = db.relationship(
+        'Customer', secondary='customer_inventory_orders', overlaps='customer_inventory, inventory_customer')
+    
+# Join Table Customer, Inventory, Order
+customer_inventory_order = db.Table(
+    'customer_inventory_orders',
+    db.Column("id", db.Integer, primary_key=True),
+    db.Column('customer_id', db.Integer, db.ForeignKey('customers.id')),
+    db.Column('product_inventory_id', db.Integer,
+              db.ForeignKey('inventory.id')),
+    db.Column('sales_order_id', db.Integer, db.ForeignKey('sales_orders.id'))
+)
