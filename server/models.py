2e5407b0e40dc1d840ea412b13a95b393c8ed490
antwoine <antwoinegriggs@outlook.com>
2023-10-02 16:31:43 -0400

    init and inventory model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,18 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class ProductInventory(db.Model, SerializerMixin):
+    __tablename__ = 'inventory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(20))
+    product_number = db.Column(db.Integer)
+    product_quantity = db.Column(db.Integer)
+    product_price = db.Column(db.Integer)
+
+    # Many-to-Many Customer
+    inventory_customer = db.relationship(
+        'Customer', secondary='customer_inventory_orders', overlaps='customer_inventory')
+
+    # One-to-Many SalesOrder
+    inventory_orders = db.relationship('SalesOrder')
