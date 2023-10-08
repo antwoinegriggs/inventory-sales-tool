41ef10955164757dfc7e04c7c56f8f61120f57a8
antwoine <antwoinegriggs@outlook.com>
2023-10-08 16:59:03 -0400

    SalesOrders React

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import * from "../pages/Inventory";
+import SalesOrders from "../pages/Sales";
+
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/check_session").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
+  return (
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        <Switch>
+          <Route path="/new">
+            <Inventory user={user} />
+          </Route>
+          <Route path="/">
+            <SalesOrders />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );;
 }
 
 export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import LoginForm from "../components/LoginForm";
+import SignUpForm from "../components/SignUpForm";
+
+function Login({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <Wrapper>
+      <Logo>IST</Logo>
+      {showLogin ? (
+        <>
+          <LoginForm onLogin={onLogin} />
+          <Divider />
+          <p>
+            Don't have an account? &nbsp;
+            <Button color="secondary" onClick={() => setShowLogin(false)}>
+              Sign Up
+            </Button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignUpForm onLogin={onLogin} />
+          <Divider />
+          <p>
+            Already have an account? &nbsp;
+            <Button color="secondary" onClick={() => setShowLogin(true)}>
+              Log In
+            </Button>
+          </p>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+export default Login;
