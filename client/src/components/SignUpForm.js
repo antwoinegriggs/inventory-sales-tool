7bf19a8f57a0b2fb8d2d106996741625f119a370
antwoine <antwoinegriggs@outlook.com>
2023-10-08 15:49:35 -0400

    signup form

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function SignUpForm({ onLogin }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      setErrors([]);
+      setIsLoading(true);
+      fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+          password_confirmation: passwordConfirmation,
+          image_url: imageUrl,
+          bio,
+        }),
+      }).then((r) => {
+        setIsLoading(false);
+        if (r.ok) {
+          r.json().then((user) => onLogin(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+      });
+    }
+  
+    return (<></>)
